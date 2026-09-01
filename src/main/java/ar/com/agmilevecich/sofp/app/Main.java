cb57ca4a05a62f34e274e9f956aa52d5d0bffc6d
agmilevecich <agmilevecich@yahoo.com.ar>
2026-09-01 13:55:17 -0300

    feat: iniciar shell Swing desde Main

--- src/main/java/ar/com/agmilevecich/sofp/app/Main.java
+++ src/main/java/ar/com/agmilevecich/sofp/app/Main.java
@@ -1,7 +1,15 @@
 package ar.com.agmilevecich.sofp.app;
 
+import ar.com.agmilevecich.sofp.ui.MainFrame;
+
+import javax.swing.SwingUtilities;
+
 public class Main {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
     }
 }
