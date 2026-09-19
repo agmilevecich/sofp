b1d6d0c15c235e7a4f977eb668feb6a46c87f651
agmilevecich <agmilevecich@yahoo.com.ar>
2026-09-19 10:27:29 -0300

    fix: importar entidad de financiacion en test

--- src/test/java/ar/com/agmilevecich/sofp/service/ObligacionServiceCierreTest.java
+++ src/test/java/ar/com/agmilevecich/sofp/service/ObligacionServiceCierreTest.java
@@ -5,6 +5,7 @@ import ar.com.agmilevecich.sofp.domain.Categoria;
 import ar.com.agmilevecich.sofp.domain.Cuenta;
 import ar.com.agmilevecich.sofp.domain.EstadoObligacion;
 import ar.com.agmilevecich.sofp.domain.FormaPago;
+import ar.com.agmilevecich.sofp.domain.Financiacion;
 import ar.com.agmilevecich.sofp.domain.InstitucionFinanciera;
 import ar.com.agmilevecich.sofp.domain.Moneda;
 import ar.com.agmilevecich.sofp.domain.Movimiento;
