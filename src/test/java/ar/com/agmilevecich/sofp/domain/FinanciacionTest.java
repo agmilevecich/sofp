4593ac26e29eb57fc1a5351fb7dff909dc69f13b
agmilevecich <agmilevecich@yahoo.com.ar>
2026-09-20 09:19:29 -0300

    test: corregir pago acumulado de financiacion

--- src/test/java/ar/com/agmilevecich/sofp/domain/FinanciacionTest.java
+++ src/test/java/ar/com/agmilevecich/sofp/domain/FinanciacionTest.java
@@ -47,7 +47,7 @@ class FinanciacionTest {
         assertEquals(new BigDecimal("100.00"), financiacion.getSaldoCargosPendiente());
         assertEquals(new BigDecimal("100000.00"), financiacion.getSaldoCapital());
 
-        financiacion.registrarPago(new BigDecimal("100200.00"));
+        financiacion.registrarPago(new BigDecimal("100100.00"));
         assertEquals(new BigDecimal("0.00"), financiacion.getSaldoCargosPendiente());
         assertEquals(new BigDecimal("0.00"), financiacion.getSaldoCapital());
         assertTrue(financiacion.estaCancelada());
