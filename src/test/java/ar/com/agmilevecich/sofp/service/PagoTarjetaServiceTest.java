35e3c9c2fb89f74d92ce59cd20dcc936ee381c97
agmilevecich <agmilevecich@yahoo.com.ar>
2026-09-20 16:54:40 -0300

    fix: importar LocalDate en test de tarjeta

--- src/test/java/ar/com/agmilevecich/sofp/service/PagoTarjetaServiceTest.java
+++ src/test/java/ar/com/agmilevecich/sofp/service/PagoTarjetaServiceTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
