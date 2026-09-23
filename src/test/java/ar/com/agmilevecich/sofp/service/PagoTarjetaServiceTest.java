0550d6248b12ecf2a8019d1328a8def430062c2b
agmilevecich <agmilevecich@yahoo.com.ar>
2026-09-23 20:01:28 -0300

    test: corregir credito esperado de pago de refinanciacion

--- src/test/java/ar/com/agmilevecich/sofp/service/PagoTarjetaServiceTest.java
+++ src/test/java/ar/com/agmilevecich/sofp/service/PagoTarjetaServiceTest.java
@@ -273,7 +273,7 @@ class PagoTarjetaServiceTest {
         ).setParameter("obligacionId", obligacion.getId()).setMaxResults(1).getSingleResult();
         assertEquals(refinanciacion.getId(), pago.getRefinanciacion().getId());
         assertEquals(new BigDecimal("81073.27"), refinanciacion.getSaldoPlan());
-        assertEquals(new BigDecimal("424000.00"), cuentaService.calcularCreditoDisponible(tarjeta.getId(), usuario.getId()));
+        assertEquals(new BigDecimal("418926.73"), cuentaService.calcularCreditoDisponible(tarjeta.getId(), usuario.getId()));
         assertEquals(new BigDecimal("150000.00"), cuentaService.calcularSaldo(cuentaPagadora.getId(), usuario.getId()));
     }
 
@@ -308,7 +308,7 @@ class PagoTarjetaServiceTest {
 
         assertEquals("REVERSADO", pago.getEstado().name());
         assertEquals(new BigDecimal("131073.27"), pago.getRefinanciacion().getSaldoPlan());
-        assertEquals(new BigDecimal("374000.00"), cuentaService.calcularCreditoDisponible(tarjeta.getId(), usuario.getId()));
+        assertEquals(new BigDecimal("368926.73"), cuentaService.calcularCreditoDisponible(tarjeta.getId(), usuario.getId()));
         assertEquals(new BigDecimal("200000.00"), cuentaService.calcularSaldo(cuentaPagadora.getId(), usuario.getId()));
     }
 
